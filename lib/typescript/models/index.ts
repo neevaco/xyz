4038f1790ec98f10cd9a23e6d6b9f600e9f8ccf7
Paul Lockett <Plockett.pl@gmail.com>
2022-08-08 15:10:48 -0700

    add fetch implmentation

---
+++ lib/typescript/models/index.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BlockchainInfo';
+export * from './Collection';
+export * from './CurrencyInfo';
+export * from './ENS';
+export * from './ErrorMessage';
+export * from './Media';
+export * from './MediaPreview';
+export * from './MediaVersion';
+export * from './NonFungibleToken';
+export * from './OpenSeaContract';
+export * from './OwnedCollection';
+export * from './SearchDocument';
+export * from './SocialMedia';
+export * from './Token';
+export * from './TokenAttribute';
+export * from './TokenEvents';
+export * from './TokenGate';
+export * from './Transaction';
+export * from './TransactionLogLine';
+export * from './URL';
+export * from './Wallet';
